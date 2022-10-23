6d69147b15f54dad9b68567ac9e44066eede91c4
dingmy002 <41277960+dingmy002@users.noreply.github.com>
2022-10-23 16:35:24 +0800

    Create SageMaker-emr-lifecycle-config.py

---
+++ SageMaker-emr-lifecycle-config.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+import boto3
+import re
+
+def fetch_emr_ip():
+	boto_emr_client = boto3.client("emr")
+	cluster_list = boto_emr_client.list_clusters(ClusterStates = ['RUNNING','WAITING'])['Clusters']
+	cluster_name = r'Sparkmagic'
+
+	for cluster in cluster_list:
+		if re.match(cluster_name, cluster['Name']):
+			cluster_id = cluster['Id']
+			master_node = boto_emr_client.list_instances(ClusterId=cluster_id,InstanceGroupTypes=['MASTER'],InstanceStates=['RUNNING'])['Instances'][0]
+			ip = master_node['PublicDnsName']
+			return ip
+
+print(fetch_emr_ip())
